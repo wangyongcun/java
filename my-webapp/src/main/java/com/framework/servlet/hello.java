d2efa0b38785f09a9c74ca28a14d4b6bb94dbee8
Benjamin Wang (yongcwan) <yongcwan@cisco.com>
2021-09-10 09:04:29 +0800

    mvn project using servlet and mvn tomcat plugin contain: 1 simple jsp http://127.0.0.1:8080/ 2 simple servlet http://127.0.0.1:8080/Helloworld

---
+++ my-webapp/src/main/java/com/framework/servlet/hello.java
@@ -0,0 +1,18 @@
+package com.framework.servlet;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "Helloworld")
+public class hello  extends HttpServlet  {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("hello this is my first app");
+    }
+}
